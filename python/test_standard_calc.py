a57fe3009592a188531a71c50682c10b025d5614
hsn200406 <114446784+hsn200406@users.noreply.github.com>
2023-08-23 18:19:17 +0400

    Update test_standard_calc.py

--- python/test_standard_calc.py
+++ python/test_standard_calc.py
@@ -1,9 +1,12 @@
 from standard_calc import bound_to_180, is_angle_between
 
+
 """ Tests for bound_to_180() """
+
 
 def test_bound_basic1():
     assert bound_to_180(0) == 0
+
 
 def test_bound_basic2():
     assert bound_to_180(155) == 155.0
