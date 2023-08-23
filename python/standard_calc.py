8ac2478fa99a137af5fbfdaa98a90ecc18497810
hsn200406 <114446784+hsn200406@users.noreply.github.com>
2023-08-23 17:40:17 +0400

    Update standard_calc.py

--- python/standard_calc.py
+++ python/standard_calc.py
@@ -1,32 +1,25 @@
 def bound_to_180(angle):
-    """Bounds the provided angle between [-180, 180) degrees.
-
-    e.g.)
-        bound_to_180(135) = 135.0
-        bound_to_180(200) = -160.0
-
-    Args:
-        angle (float): The input angle in degrees.
-
-    Returns:
-        float: The bounded angle in degrees.
-    """
-    return 0
+    while angle < -180:
+        angle += 360
+    while angle > 360:
+        angle -= 360
+    return float(angle)
 
 
 def is_angle_between(first_angle, middle_angle, second_angle):
-    """Determines whether an angle is between two other angles.
+    if first_angle < middle_angle < second_angle:
+        return True
+    elif first_angle < second_angle < middle_angle:
+        return True
+    elif second_angle < middle_angle < first_angle:
+        return True
+    elif second_angle < first_angle < middle_angle:
+        return True
+    elif middle_angle < first_angle < second_angle:
+        return True
+    elif middle_angle < second_angle < first_angle:
+        return True
+    else:
+        return False
 
-    e.g.)
-        is_angle_between(0, 45, 90) = True
-        is_angle_between(45, 90, 270) = False
 
-    Args:
-        first_angle (float): The first bounding angle in degrees.
-        middle_angle (float): The angle in question in degrees.
-        second_angle (float): The second bounding angle in degrees.
-
-    Returns:
-        bool: True when `middle_angle` is not in the reflex angle of `first_angle` and `second_angle`, false otherwise.
-    """
-    return True
